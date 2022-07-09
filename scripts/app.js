5a8a3c31444ebed8a2ce634aefc3b3522d8fa3e5
Sawant Kumar <kumar.sawant37@gmail.com>
2022-07-09 23:30:36 +0530

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -11,3 +11,18 @@ function changeTextColor(color){
     const star= document.querySelector('.star');
     star.style.text = color;
 }
+
+const icon =document.querySelector('.clicker');
+const icon1 =document.querySelector('.clicker1');
+const icon2 =document.querySelector('.clicker2');
+const span=document.querySelector('span')
+
+icon.addEventListener('click',()=>{
+    span.style.color="#017143";
+})
+icon1.addEventListener('click',()=>{
+    span.style.color="#eb7495";
+})
+icon2.addEventListener('click',()=>{
+    span.style.color="#d752b1";
+})
